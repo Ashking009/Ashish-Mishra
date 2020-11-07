20709ed1954cfe478cf2891212c427bf9debd4b6
Ashking009 <mishrashish0@gmail.com>
2020-11-07 15:13:37 +0530

    aos

--- js/main.js
+++ js/main.js
@@ -75,6 +75,20 @@ $(document).ready(function(){
       $("body").removeClass("nav-open");
     });
   });
+
+
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      // once: true,
+      // mirror: false
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
 })(jQuery);
 
 
